bd38dd3024a9df4c61eaf8f02b1f33e9eb0cd599
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-07-07 19:58:38 -0700

    add persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,6 +212,50 @@ var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function () {
+// Gets task items from localStorage.
+tasks = localStorage.getItem("tasks")
+
+if (tasks === null) {
+  tasks = [];
+  return false;
+}
+// Converts tasks from the string format back into an array of objects.
+tasks = JSON.parse(tasks);
+// Iterates through a tasks array and creates task elements on the page from it.
+for (var i = 0; i < tasks.length; i++) {
+  var task = tasks[i];
+
+  task.id = taskIdCounter;
+
+  var listItemEl = document.createElement("li")
+  listItemEl.className = "task-item";
+  listItemEl.setAttribute("data-task-id", task.id);
+
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + task.name + "</h3><span class='task-type'>" + task.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  taskActionsEl = createTaskActions(task.id);
+  listItemEl.appendChild(taskActionsEl);
+
+  if (task.status === "to do") {
+    listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+    tasksToDoEl.appendChild(listItemEl);
+  } else if (task.status === "in progress") {
+    listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+    tasksInProgressEl.appendChild(listItemEl);
+  } else if (task.status === "complete") {
+    listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+    tasksCompletedEl.appendChild(listItemEl);
+  }
+  taskIdCounter++;
+  console.log(task.status);
+  console.log(listItemEl);
+}
+};
+
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -220,3 +264,5 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
